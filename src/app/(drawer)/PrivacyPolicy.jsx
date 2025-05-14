bc4a26deb706ee344bf43e6a85ca4ca53734ef2d
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-14 11:53:51 +0600

    Created the PrivacyPolicy page

--- src/app/(drawer)/PrivacyPolicy.jsx
+++ src/app/(drawer)/PrivacyPolicy.jsx
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { Text, View } from "react-native";
+import tw from 'twrnc';
 
 const PrivacyPolicy = () => {
   return (
-    <View>
-      <Text>PrivacyPolicy</Text>
+    <View style={tw`p-[6%] `}>
+      <Text style={tw``}>
+        Lorem ipsum dolor sit amet consectetur. Pretium aliquam diam sed neque
+        purus. Sed risus et turpis laoreet aliquet facilisis. Viverra urna arcu
+        faucibus at ut orci velit. Velit pellentesque lacus eu tortor eget
+        cursus suspendisse imperdiet. Lorem ipsum dolor sit amet consectetur.
+        Pretium aliquam diam sed neque purus. Sed risus et turpis laoreet
+        aliquet facilisis. Viverra urna arcu faucibus at ut orci velit. Velit
+        pellentesque lacus eu tortor eget cursus suspendisse imperdiet. Lorem
+        ipsum dolor sit amet consectetur. Pretium aliquam diam sed neque purus.
+        Sed risus et turpis laoreet aliquet facilisis. Viverra urna arcu
+        faucibus at ut orci velit. Velit pellentesque lacus eu tortor eget
+        cursus suspendisse imperdiet.
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;

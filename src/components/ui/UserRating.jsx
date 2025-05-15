56d8164ce7b5db3fd93a4892bafe4048bb2c9e0b
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-15 14:27:05 +0600

    upsated Ui

--- src/components/ui/UserRating.jsx
+++ src/components/ui/UserRating.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
 import tw from "twrnc";
 
@@ -14,7 +14,7 @@ const RatingComponent = () => {
 
   return (
     <View style={tw` flex-row items-center  gap-1 `}>
-      <Text style={tw`text-xl text-black`}>Rate :</Text>
+      <Text style={tw`text-[16px] font-bold text-[#121212] `}>Rate :</Text>
       <View style={tw`flex-row`}>
         {[1, 2, 3, 4, 5].map((star) => (
           <TouchableOpacity

8fda9bc5cd008ca038d7f89bd6afc41797a8428e
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-16 14:24:47 +0600

    created the Logo out page

---
+++ src/components/ui/Logout.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import tw from 'twrnc'
+
+const Logout = () => {
+  return (
+    <View>
+      <Text style={tw` text-[#E53E3E]`}>Log out</Text>
+    </View>
+  )
+}
+
+export default Logout

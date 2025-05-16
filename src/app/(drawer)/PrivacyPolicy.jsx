04a2dc4396f0b4b2a1d322f69db61ae04e133c3e
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-16 13:10:22 +0600

    added the back button

--- src/app/(drawer)/PrivacyPolicy.jsx
+++ src/app/(drawer)/PrivacyPolicy.jsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 import tw from 'twrnc';
 
 const PrivacyPolicy = () => {
+    const navigate = useNavigation();
+
   return (
     <View style={tw`p-[6%] `}>
+      <TouchableOpacity onPress={() => navigate.goBack()}>
+        <View style={tw`flex-row items-center gap-2 my-4`}>
+          <MaterialIcons name="arrow-back-ios" size={24} color="black" />
+          <Text style={tw`text-4.2 text-[#121212] font-bold`}>
+            Privacy policy
+          </Text>
+        </View>
+      </TouchableOpacity>
       <Text style={tw``}>
         Lorem ipsum dolor sit amet consectetur. Pretium aliquam diam sed neque
         purus. Sed risus et turpis laoreet aliquet facilisis. Viverra urna arcu

f019fd13f19374e686850ff2d62eda753659820f
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-18 11:20:05 +0600

    When tag is deleted and same tag is used again, index issue was fixed

--- src/components/ui/TagManager.jsx
+++ src/components/ui/TagManager.jsx
@@ -1,33 +1,37 @@
-import { useEffect, useState } from 'react';
-import { Pressable, ScrollView, Text, View } from 'react-native';
-import tw from 'twrnc';
+import { useEffect, useState } from "react";
+import { Pressable, ScrollView, Text, View } from "react-native";
+import tw from "twrnc";
 
-const TagManager = ({ newTag }) => {
+const TagManager = ({ newTag, setNewTag }) => {
   const [tags, setTags] = useState([]);
 
-//   console.log(new);
-  
-  
   useEffect(() => {
     if (newTag && !tags.includes(newTag)) {
-      setTags([...tags, newTag]);
+      setTags((prev) => [...prev, newTag]);
     }
   }, [newTag]);
 
- 
   const removeTag = (tagToRemove) => {
-    setTags(tags.filter(tag => tag !== tagToRemove));
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+
+    if (newTag === tagToRemove && setNewTag) {
+      setNewTag("");
+    }
   };
 
   return (
-    <View style={tw`p-4`}>
-      <Text style={tw`font-bold text-base mb-2`}>Tagged with:</Text>
+    <View style={tw`flex-1 flex-row justify-center items-center gap-2`}>
+      {tags.length > 0 && (
+        <Text style={tw`font-semibold text-4 text-[#121212]`}>
+          Tagged with :
+        </Text>
+      )}
 
-      <ScrollView horizontal contentContainerStyle={tw`flex-row flex-wrap gap-2`}>
+      <ScrollView horizontal contentContainerStyle={tw`flex-row gap-2`}>
         {tags.map((tag, index) => (
           <View
             key={index}
-            style={tw`flex-row items-center px-3 py-1 border border-blue-500 rounded-full bg-gray-100`}
+            style={tw`flex-row items-center px-3 py-1 border border-[#0063E5] rounded-2 bg-gray-100`}
           >
             <Text style={tw`text-gray-600 mr-2`}>{tag}</Text>
             <Pressable onPress={() => removeTag(tag)}>

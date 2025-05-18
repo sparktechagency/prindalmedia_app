ad9ff897d72362904ef685e1a816f0ca94f177a8
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-18 08:52:33 +0600

    removed the bg-image and added bg-color

--- src/app/auth/EmailVerify.jsx
+++ src/app/auth/EmailVerify.jsx
@@ -1,12 +1,10 @@
 import { Fontisto } from "@expo/vector-icons";
-import { ImageBackground } from "expo-image";
 import { router } from "expo-router";
 import { Formik } from "formik";
 import { useState } from "react";
-import { Pressable, Text, TextInput, View } from "react-native";
+import { Pressable, SafeAreaView, Text, TextInput, View } from "react-native";
 import tw from "twrnc";
 import * as Yup from "yup";
-import image from "../../../assets/images/SplashScreen.png";
 import AuthHeading from "../../components/ui/AuthHeading";
 
 const ForgotPasswordSchema = Yup.object().shape({
@@ -24,73 +22,67 @@ const EmailVerify = () => {
   };
 
   return (
-     <View >
-       <ImageBackground
-        source={image}
-        style={tw`w-full  overflow-hidden`}
-      >
-        <View
-          style={tw`p-[4%] h-full  dark:bg-black flex-col items-center justify-center `}
+    <View style={tw`p-[4%] bg-[#FDFFFE] flex-1 justify-center `}>
+      <SafeAreaView style={tw` dark:bg-black flex-col items-center `}>
+        {/*  */}
+
+        <AuthHeading
+          Heading={"Forgot password"}
+          SubHeading={" Enter your email address to reset your password"}
+        />
+
+        <Formik
+          initialValues={{ email: "" }}
+          validationSchema={ForgotPasswordSchema}
+          onSubmit={handleFormSubmit}
         >
-          {/*  */}
-
-          <AuthHeading Heading={'Forgot password'} SubHeading={' Enter your email address to reset your password'} />
-
-          <Formik
-            initialValues={{ email: "" }}
-            validationSchema={ForgotPasswordSchema}
-            onSubmit={handleFormSubmit}
-          >
-            {({
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              values,
-              errors,
-              touched,
-            }) => (
-              <>
-                {/* Email input */}
-                <View style={tw`mt-2 w-full flex-col gap-2`}>
-                  <Text style={tw`text-lg font-medium`}>Email</Text>
-                  <View
-                    style={tw`flex-row justify-between items-center bg-[#F3F3F3] rounded-md px-4 py-2`}
-                  >
-                    <Fontisto
-                      name="email"
-                      size={20}
-                      color="black"
-                      style={tw`mr-2 text-[#888888]`}
-                    />
-                    <TextInput
-                      style={tw`flex-1 text-base text-black dark:text-white`}
-                      onChangeText={handleChange("email")}
-                      onBlur={handleBlur("email")}
-                      value={values.email}
-                      keyboardType="email-address"
-                      autoCapitalize="none"
-                    />
-                  </View>
-                  {touched.email && errors.email && (
-                    <Text style={tw`text-red-500`}>{errors.email}</Text>
-                  )}
-                </View>
-
-                {/* Submit button */}
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+          }) => (
+            <>
+              {/* Email input */}
+              <View style={tw`mt-2 w-full flex-col gap-2`}>
+                <Text style={tw`text-lg font-medium`}>Email</Text>
                 <View
-                  style={tw`w-full flex-col gap-4 mt-10 rounded-full bg-[#121212]`}
+                  style={tw`flex-row justify-between items-center bg-[#F3F3F3] rounded-md px-4 py-2`}
                 >
-                  <Pressable onPress={handleSubmit} style={tw`py-4`}>
-                    <Text style={tw`text-center text-white text-xl`}>
-                      Submit
-                    </Text>
-                  </Pressable>
+                  <Fontisto
+                    name="email"
+                    size={20}
+                    color="black"
+                    style={tw`mr-2 text-[#888888]`}
+                  />
+                  <TextInput
+                    style={tw`flex-1 text-base text-black dark:text-white`}
+                    onChangeText={handleChange("email")}
+                    onBlur={handleBlur("email")}
+                    value={values.email}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                  />
                 </View>
-              </>
-            )}
-          </Formik>
-        </View>
-      </ImageBackground>
+                {touched.email && errors.email && (
+                  <Text style={tw`text-red-500`}>{errors.email}</Text>
+                )}
+              </View>
+
+              {/* Submit button */}
+              <View
+                style={tw`w-full flex-col gap-4 mt-10 rounded-full bg-[#121212]`}
+              >
+                <Pressable onPress={handleSubmit} style={tw`py-4`}>
+                  <Text style={tw`text-center text-white text-xl`}>Submit</Text>
+                </Pressable>
+              </View>
+            </>
+          )}
+        </Formik>
+      </SafeAreaView>
     </View>
   );
 };

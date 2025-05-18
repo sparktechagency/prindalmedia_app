00d8f0117ee4580ca5de8b7092fcfe560ff819b8
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-18 08:59:04 +0600

    removed the bg-image and added bg-color and updated the checkbox

--- src/app/auth/OTPVerifyTow.jsx
+++ src/app/auth/OTPVerifyTow.jsx
@@ -1,10 +1,8 @@
-import { ImageBackground } from "expo-image";
-import { router } from "expo-router";
+import { Link, router } from "expo-router";
 import { useState } from "react";
-import { Alert, Pressable, Text, View } from "react-native";
+import { Alert, Pressable, SafeAreaView, Text, View } from "react-native";
 import { OtpInput } from "react-native-otp-entry";
 import tw from "twrnc";
-import image from "../../../assets/images/SplashScreen.png";
 import AuthHeading from "../../components/ui/AuthHeading";
 
 const OTPVerifyTow = () => {
@@ -19,44 +17,47 @@ const OTPVerifyTow = () => {
   };
 
   return (
-    <View>
-      <ImageBackground
-        source={image}
-        resizeMode="cover"
-        style={tw`w-full  overflow-hidden`}
-      >
-        <View
-          style={tw`flex flex-col   items-center justify-center h-full p-[4%] `}
-        >
-          
+    <View style={tw`bg-[#FDFFFE] justify-center h-full p-[4%]`}>
+      <SafeAreaView>
+        <View style={tw`flex flex-col `}>
           {/*   */}
-          <AuthHeading Heading={'Verify OTP'} SubHeading={'We have sent a six digit code to your email'} />
-          <OtpInput
-            style={tw`bg-red-500`}
-            focusColor="black"
-            placeholder="******"
-            numberOfDigits={6}
-            type="numeric"
-            onFilled={(text) => setOtpVerify(text)}
-            textInputProps={{
-              accessibilityLabel: "One-Time Password",
-            }}
-            textProps={{
-              accessibilityRole: "text",
-              accessibilityLabel: "OTP digit",
-              allowFontScaling: false,
-            }}
+          <AuthHeading
+            Heading={"Verify OTP"}
+            SubHeading={"We have sent a six digit code to your email"}
           />
+          <View style={tw`flex flex-col gap-2 justify-end items-end `}>
+            <OtpInput
+              style={tw`bg-[#F3F3F3]`}
+              focusColor="black"
+              placeholder="000000"
+              numberOfDigits={6}
+              type="numeric"
+              onFilled={(text) => setOtpVerify(text)}
+              textInputProps={{
+                accessibilityLabel: "One-Time Password",
+              }}
+              textProps={{
+                accessibilityRole: "text",
+                accessibilityLabel: "OTP digit",
+                allowFontScaling: false,
+              }}
+            />
+            {/* navigation */}
+            <Link
+              href=""
+              style={tw`text-[#0063E5] text-sm font-semibold underline `}
+            >
+              Send again?
+            </Link>
+          </View>
 
-          <View
-            style={tw`w-full flex-col rounded-full bg-[#121212] mt-10 `}
-          >
+          <View style={tw`w-full flex-col rounded-full bg-[#121212] mt-10 `}>
             <Pressable onPress={handleNavigate} style={tw`py-4`}>
               <Text style={tw`text-center text-white text-xl`}>Verify</Text>
             </Pressable>
           </View>
         </View>
-      </ImageBackground>
+      </SafeAreaView>
     </View>
   );
 };

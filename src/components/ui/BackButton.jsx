c704a29811501f8887f699109fefd18fe03cd06f
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-19 11:59:58 +0600

    custom back button

---
+++ src/components/ui/BackButton.jsx
@@ -0,0 +1,21 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+import tw from "../../lib/tailwind";
+
+const BackButton = ({ title }) => {
+  const navigate = useNavigation();
+
+  return (
+    <View style={tw``}>
+      <TouchableOpacity onPress={() => navigate.goBack()}>
+        <View style={tw`flex-row items-center gap-2 my-4`}>
+          <MaterialIcons name="arrow-back-ios" size={24} color="black" />
+          <Text style={tw`text-5 text-[#121212] font-inter-700`}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BackButton;

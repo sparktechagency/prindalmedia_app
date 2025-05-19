44811c2280f40faced3b9af8377a51a7c30e116f
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-19 08:40:41 +0600

    created the new Select Radio Button  components

---
+++ src/components/ui/SelectRadioButton.jsx
@@ -0,0 +1,63 @@
+import { Text, View } from "react-native";
+import { RadioButton } from "react-native-ui-lib";
+import tw from "twrnc";
+
+const SelectRadioButton = ({
+  setSelectedOption,
+  selectedOption,
+  setSelectedOptionFood,
+  selectedOptionFood,
+}) => {
+  return (
+    <View style={tw`flex-col  gap-4`}>
+      <View style={tw`gap-2`}>
+        <Text style={tw`text-[16px] font-semibold text-[#121212]`}>
+          Where did you have it?
+        </Text>
+        <View style={tw`flex-row gap-6`}>
+          <RadioButton
+            label="Restaurant"
+            labelStyle={{ color: "#454545", fontWeight: "400" }}
+            selected={selectedOption === "restaurant"}
+            onPress={() => setSelectedOption("restaurant")}
+            color="#B0B0B0"
+          />
+
+          <RadioButton
+            label="Home-Made"
+            labelStyle={{ color: "#454545", fontWeight: "400" }}
+            selected={selectedOption === "home-made"}
+            onPress={() => setSelectedOption("home-made")}
+            color="#B0B0B0"
+          />
+        </View>
+      </View>
+      <View style={tw`gap-2`}>
+        <Text style={tw`text-[16px] font-semibold text-[#121212]`}>
+          Food type?
+        </Text>
+        <View style={tw`flex-row gap-2`}>
+          <View style={tw`flex-row gap-6`}>
+            <RadioButton
+              label="Meal"
+              labelStyle={{ color: "#454545", fontWeight: "400" }}
+              selected={selectedOptionFood === "meal"}
+              onPress={() => setSelectedOptionFood("meal")}
+              color="#B0B0B0"
+            />
+
+            <RadioButton
+              label="Drink"
+              labelStyle={{ color: "#454545", fontWeight: "400" }}
+              selected={selectedOptionFood === "drink"}
+              onPress={() => setSelectedOptionFood("drink")}
+              color="#B0B0B0"
+            />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default SelectRadioButton;

d9ff786df2850e56bc59d0aa626a2da2fa675967
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-19 14:48:06 +0600

    add  useSafeAreaInsets full page

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,10 +1,18 @@
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native";
-import tw from "twrnc";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  const { bottom, top } = useSafeAreaInsets();
+
   return (
-    <SafeAreaView style={tw`flex-1 mt-5`}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingBottom: bottom,
+        paddingTop: top,
+      }}
+    >
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" options={{}} />
         <Stack.Screen name="auth/EmailVerify" options={{}} />

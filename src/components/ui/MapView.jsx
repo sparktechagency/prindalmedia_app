c60128d1a9b33377a198d79c45a78eb37aab4a38
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-19 14:35:13 +0600

    added the Expo map

--- src/components/ui/MapView.jsx
+++ src/components/ui/MapView.jsx
@@ -1,10 +1,12 @@
+import { AppleMaps, GoogleMaps } from "expo-maps";
+import { Platform, Text } from "react-native";
 
-const MapView = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default MapView;
+export default function MapView() {
+  if (Platform.OS === "ios") {
+    return <AppleMaps.View style={{ flex: 1 }} />;
+  } else if (Platform.OS === "android") {
+    return <GoogleMaps.View style={{ flex: 1 }} />;
+  } else {
+    return <Text>Maps are only available on Android and iOS</Text>;
+  }
+}

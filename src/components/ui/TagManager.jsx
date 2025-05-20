55ef710735b1f97aed36e63f00c35fa7949e34bc
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-20 12:46:51 +0600

    updated the ui style

--- src/components/ui/TagManager.jsx
+++ src/components/ui/TagManager.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Pressable, ScrollView, Text, View } from "react-native";
-import tw from "twrnc";
+import tw from "../../lib/tailwind";
 
 const TagManager = ({ newTag, setNewTag }) => {
   const [tags, setTags] = useState([]);
@@ -22,7 +22,7 @@ const TagManager = ({ newTag, setNewTag }) => {
   return (
     <View style={tw`flex-1 flex-row justify-center items-center gap-2`}>
       {tags.length > 0 && (
-        <Text style={tw`font-semibold text-4 text-[#121212]`}>
+        <Text style={tw`text-3 font-inter-600 text-[#121212]`}>
           Tagged with :
         </Text>
       )}
@@ -33,9 +33,11 @@ const TagManager = ({ newTag, setNewTag }) => {
             key={index}
             style={tw`flex-row items-center px-3 py-1 border border-[#0063E5] rounded-2 bg-gray-100`}
           >
-            <Text style={tw`text-gray-600 mr-2`}>{tag}</Text>
+            <Text style={tw`text-3 font-inter-600 text-[#888888] mr-2`}>
+              {tag}
+            </Text>
             <Pressable onPress={() => removeTag(tag)}>
-              <Text style={tw`text-gray-500 text-lg`}>×</Text>
+              <Text style={tw`text-[#888888] text-lg`}>×</Text>
             </Pressable>
           </View>
         ))}

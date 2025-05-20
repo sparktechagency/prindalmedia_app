b8c5aa09445bd18b9c3f636739a54857b85fbbac
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-20 10:25:02 +0600

    added TouchableOpacity  button

--- src/components/ui/RecentActivityList.jsx
+++ src/components/ui/RecentActivityList.jsx
@@ -1,5 +1,5 @@
 import { FontAwesome } from "@expo/vector-icons";
-import { FlatList, Image, Text, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import foodImage from "../../../assets/images/food-image.png";
 import Icon from "../../../assets/images/restaurant.png";
 import tw from "../../lib/tailwind";
@@ -16,57 +16,64 @@ const RecentActivityList = () => {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={tw`pb-6`}
         renderItem={({ item, index }) => (
-          <View style={tw`flex-col my-2 justify-between items-center`}>
-            <View
-              style={tw`flex-row gap-4 bg-[#D5D5D51A] p-2 rounded-2xl items-center`}
-            >
-              {/* Image */}
-              <Image source={foodImage} style={tw`w-18 h-18 rounded-[8px]`} />
+          <TouchableOpacity>
+            <View style={tw`flex-col my-2 justify-between items-center`}>
+              <View
+                style={tw`flex-row gap-4 bg-[#D5D5D51A] p-2 rounded-2xl items-center`}
+              >
+                {/* Image */}
+                <Image source={foodImage} style={tw`w-18 h-18 rounded-[8px]`} />
 
-              {/* Content */}
-              <View style={tw`flex-1 px-2`}>
-                {/* Title and Rating */}
-                <View style={tw`flex-row justify-between items-center`}>
-                  <Text style={tw`text-base  font-inter-700 text-textPrimary`}>
-                    Spaghetti Carbonara
-                  </Text>
-                  <View style={tw`flex-row items-center`}>
-                    <FontAwesome name="star" size={16} color="#facc15" />
-                    <Text style={tw`ml-1 text-textPrimary font-inter-600 `}>
-                      2.8
-                    </Text>
-                  </View>
-                </View>
-
-                {/* Location and Date */}
-                <View style={tw`flex-row justify-between items-center mt-1`}>
-                  <View style={tw`flex-row items-center`}>
-                    <Image style={tw`w-4 h-4`} source={Icon} />
+                {/* Content */}
+                <View style={tw`flex-1 px-2`}>
+                  {/* Title and Rating */}
+                  <View style={tw`flex-row justify-between items-center`}>
                     <Text
-                      style={tw`text-[#454545] ml-1 font-inter-400 text-sm`}
+                      style={tw`text-base  font-inter-700 text-textPrimary`}
                     >
-                      78 Tampa, Florida
+                      Spaghetti Carbonara
+                    </Text>
+                    <View style={tw`flex-row items-center`}>
+                      <FontAwesome name="star" size={16} color="#facc15" />
+                      <Text style={tw`ml-1 text-textPrimary font-inter-600 `}>
+                        2.8
+                      </Text>
+                    </View>
+                  </View>
+
+                  {/* Location and Date */}
+                  <View style={tw`flex-row justify-between items-center mt-1`}>
+                    <View style={tw`flex-row items-center`}>
+                      <Image style={tw`w-4 h-4`} source={Icon} />
+                      <Text
+                        style={tw`text-[#454545] ml-1 font-inter-400 text-sm`}
+                      >
+                        78 Tampa, Florida
+                      </Text>
+                    </View>
+                    <Text style={tw`text-[#454545] font-inter-400 text-sm`}>
+                      07/04/2027
                     </Text>
                   </View>
-                  <Text style={tw`text-[#454545] font-inter-400 text-sm`}>
-                    07/04/2027
-                  </Text>
-                </View>
-
-                {/* Tags */}
-                <View style={tw`flex-row justify-between items-center mt-2`}>
-                  <View style={tw`flex-row gap-2`}>
-                    <Text style={tw`text-[12px] font-inter-600 text-[#454545]`}>
-                      Meal
-                    </Text>
-                    <Text style={tw`text-[12px] font-inter-600 text-[#454545]`}>
-                      Restaurant
-                    </Text>
+                  {/* Tags */}
+                  <View style={tw`flex-row justify-between items-center mt-2`}>
+                    <View style={tw`flex-row gap-2`}>
+                      <Text
+                        style={tw`text-[12px] font-inter-600 text-[#454545]`}
+                      >
+                        Meal
+                      </Text>
+                      <Text
+                        style={tw`text-[12px] font-inter-600 text-[#454545]`}
+                      >
+                        Restaurant
+                      </Text>
+                    </View>
                   </View>
                 </View>
               </View>
             </View>
-          </View>
+          </TouchableOpacity>
         )}
       />
     </View>

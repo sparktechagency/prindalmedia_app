6e17aee027145bcf4cff4d1298f6f94502468c66
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-20 10:18:08 +0600

    added the close icons and updated the style

--- src/components/ui/ButtomSheet.jsx
+++ src/components/ui/ButtomSheet.jsx
@@ -1,17 +1,18 @@
-import { AntDesign, Feather, FontAwesome6 } from "@expo/vector-icons";
+import { AntDesign, FontAwesome6, Ionicons } from "@expo/vector-icons";
 import { useRef, useState } from "react";
-import { Image } from "react-native";
 import {
-  View,
-  Text,
+  Image,
   Pressable,
   SafeAreaView,
   ScrollView,
+  Text,
   TouchableOpacity,
+  View,
 } from "react-native";
+
 import { TextInput } from "react-native-gesture-handler";
 import RBSheet from "react-native-raw-bottom-sheet";
-import tw from "twrnc";
+import tw from "../../lib/tailwind";
 export default function ButtomSheet() {
   const bottomSheetRef = useRef();
 
@@ -19,14 +20,18 @@ export default function ButtomSheet() {
     bottomSheetRef.current.open();
   };
 
-  const [comment, setComment] = useState('');
-//   console.log(comment);
+  const [comment, setComment] = useState("");
+  //   console.log(comment);
 
-  // close buttom sheet 
+  // commment and  close buttom sheet
   const handleComment = () => {
-    setComment('')
+    setComment("");
     bottomSheetRef.current.close();
-  }
+  };
+  // close buttom sheet
+  const handlClose = () => {
+    bottomSheetRef.current.close();
+  };
 
   return (
     <SafeAreaView>
@@ -55,227 +60,98 @@ export default function ButtomSheet() {
           },
         }}
       >
-        <View style={tw`text-lg font-medium py-3`}>
-          <Text style={tw`text-lg font-medium py-3`}>Comment</Text>
+        <View style={tw``}>
+          <View style={tw` flex-row justify-between items-center`}>
+            <Text style={tw`text-lg font-inter-700  py-3`}>Comment</Text>
+            <Ionicons
+              name="close"
+              size={24}
+              //   style={tw`mr-2 text-[#00C49A]`}
+              color="#121212"
+              onPress={handlClose}
+            />
+          </View>
 
           <ScrollView
             showsVerticalScrollIndicator={false}
             style={{ height: 280 }}
           >
-            {/* user info */}
-            <View style={tw`flex-row items-start justify-between p-3 `}>
-              {/* Left: Avatar + Content */}
-              <View style={tw`flex-row flex-1`}>
-                <Image
-                  source={{
-                    uri: "https://randomuser.me/api/portraits/men/1.jpg",
-                  }}
-                  style={tw`w-10 h-10 rounded-full mr-3`}
-                />
-                <View style={tw`flex-1`}>
-                  <Text style={tw`font-bold text-black`}>Casey</Text>
-                  <Text style={tw`text-black text-sm mt-0.5`}>
-                    Its a nice food. Very tasty & sweet.
-                  </Text>
-                  <View style={tw`flex-row mt-1`}>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs mr-4`}>Reply</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs`}>Like</Text>
-                    </TouchableOpacity>
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
+              <View key={index}>
+                {/* user info */}
+                <View style={tw`flex-row items-start justify-between my-2 `}>
+                  {/* Left: Avatar + Content */}
+                  <View style={tw`flex-row flex-1`}>
+                    <Image
+                      source={{
+                        uri: "https://randomuser.me/api/portraits/men/1.jpg",
+                      }}
+                      style={tw`w-10.5 h-10.5 rounded-full mr-3`}
+                    />{" "}
+                    <View style={tw`flex-1`}>
+                      <Text style={tw` text-textgray font-inter-600 text-3`}>
+                        Casey
+                      </Text>
+                      <Text
+                        style={tw`text-textPrimary text-4 font-inter-400 mt-0.5`}
+                      >
+                        Its a nice food. Very tasty & sweet.
+                      </Text>
+                      <View style={tw`flex-row mt-1`}>
+                        <TouchableOpacity>
+                          <Text
+                            style={tw`font-inter-400 text-[#888888] text-xs mr-4`}
+                          >
+                            Reply
+                          </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                          <Text
+                            style={tw`font-inter-400 text-[#888888] text-xs`}
+                          >
+                            Like
+                          </Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
                   </View>
+
+                  {/* Right: Delete Icon */}
+                  <TouchableOpacity>
+                    <AntDesign name="delete" size={16} color="#666" />
+                  </TouchableOpacity>
                 </View>
               </View>
-
-              {/* Right: Delete Icon */}
-              <TouchableOpacity>
-                <AntDesign name="delete" size={16} color="#666" />
-              </TouchableOpacity>
-            </View>
-            {/* user info */}
-            <View style={tw`flex-row items-start justify-between p-3 `}>
-              {/* Left: Avatar + Content */}
-              <View style={tw`flex-row flex-1`}>
-                <Image
-                  source={{
-                    uri: "https://randomuser.me/api/portraits/men/1.jpg",
-                  }}
-                  style={tw`w-10 h-10 rounded-full mr-3`}
-                />
-                <View style={tw`flex-1`}>
-                  <Text style={tw`font-bold text-black`}>Casey</Text>
-                  <Text style={tw`text-black text-sm mt-0.5`}>
-                    Its a nice food. Very tasty & sweet.
-                  </Text>
-                  <View style={tw`flex-row mt-1`}>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs mr-4`}>Reply</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs`}>Like</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-
-              {/* Right: Delete Icon */}
-              <TouchableOpacity>
-                <AntDesign name="delete" size={16} color="#666" />
-              </TouchableOpacity>
-            </View>
-            {/* user info */}
-            <View style={tw`flex-row items-start justify-between p-3 `}>
-              {/* Left: Avatar + Content */}
-              <View style={tw`flex-row flex-1`}>
-                <Image
-                  source={{
-                    uri: "https://randomuser.me/api/portraits/men/1.jpg",
-                  }}
-                  style={tw`w-10 h-10 rounded-full mr-3`}
-                />
-                <View style={tw`flex-1`}>
-                  <Text style={tw`font-bold text-black`}>Casey</Text>
-                  <Text style={tw`text-black text-sm mt-0.5`}>
-                    Its a nice food. Very tasty & sweet.
-                  </Text>
-                  <View style={tw`flex-row mt-1`}>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs mr-4`}>Reply</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs`}>Like</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-
-              {/* Right: Delete Icon */}
-              <TouchableOpacity>
-                <AntDesign name="delete" size={16} color="#666" />
-              </TouchableOpacity>
-            </View>
-            {/* user info */}
-            <View style={tw`flex-row items-start justify-between p-3 `}>
-              {/* Left: Avatar + Content */}
-              <View style={tw`flex-row flex-1`}>
-                <Image
-                  source={{
-                    uri: "https://randomuser.me/api/portraits/men/1.jpg",
-                  }}
-                  style={tw`w-10 h-10 rounded-full mr-3`}
-                />
-                <View style={tw`flex-1`}>
-                  <Text style={tw`font-bold text-black`}>Casey</Text>
-                  <Text style={tw`text-black text-sm mt-0.5`}>
-                    Its a nice food. Very tasty & sweet.
-                  </Text>
-                  <View style={tw`flex-row mt-1`}>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs mr-4`}>Reply</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs`}>Like</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-
-              {/* Right: Delete Icon */}
-              <TouchableOpacity>
-                <AntDesign name="delete" size={16} color="#666" />
-              </TouchableOpacity>
-            </View>
-            {/* user info */}
-            <View style={tw`flex-row items-start justify-between p-3 `}>
-              {/* Left: Avatar + Content */}
-              <View style={tw`flex-row flex-1`}>
-                <Image
-                  source={{
-                    uri: "https://randomuser.me/api/portraits/men/1.jpg",
-                  }}
-                  style={tw`w-10 h-10 rounded-full mr-3`}
-                />
-                <View style={tw`flex-1`}>
-                  <Text style={tw`font-bold text-black`}>Casey</Text>
-                  <Text style={tw`text-black text-sm mt-0.5`}>
-                    Its a nice food. Very tasty & sweet.
-                  </Text>
-                  <View style={tw`flex-row mt-1`}>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs mr-4`}>Reply</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs`}>Like</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-
-              {/* Right: Delete Icon */}
-              <TouchableOpacity>
-                <AntDesign name="delete" size={16} color="#666" />
-              </TouchableOpacity>
-            </View>
-            {/* user info */}
-            <View style={tw`flex-row items-start justify-between p-3 `}>
-              {/* Left: Avatar + Content */}
-              <View style={tw`flex-row flex-1`}>
-                <Image
-                  source={{
-                    uri: "https://randomuser.me/api/portraits/men/1.jpg",
-                  }}
-                  style={tw`w-10 h-10 rounded-full mr-3`}
-                />
-                <View style={tw`flex-1`}>
-                  <Text style={tw`font-bold text-black`}>Casey</Text>
-                  <Text style={tw`text-black text-sm mt-0.5`}>
-                    Its a nice food. Very tasty & sweet.
-                  </Text>
-                  <View style={tw`flex-row mt-1`}>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs mr-4`}>Reply</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                      <Text style={tw`text-gray-500 text-xs`}>Like</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-
-              {/* Right: Delete Icon */}
-              <TouchableOpacity>
-                <AntDesign name="delete" size={16} color="#666" />
-              </TouchableOpacity>
-            </View>
+            ))}
           </ScrollView>
           {/* User Comment  */}
-          <View style={tw` w-full flex-col gap-3 my-4`}>
+          <View style={tw`  `}>
             <View
-              style={tw`flex-row justify-between items-center bg-[#F3F3F3] rounded-[16] px-4 py-1.2`}
+              style={tw`flex-row justify-between items-center bg-[#F3F3F3] rounded-full mt-15 p-4`}
             >
               {/* Lock icon + Input */}
               <View style={tw`flex-row items-center flex-1`}>
-                <AntDesign
-                  style={tw`mr-2 text-[#888888]`}
-                  name="user"
-                  size={20}
-                  color="black"
+                <Image
+                  source={{
+                    uri: "https://randomuser.me/api/portraits/men/1.jpg",
+                  }}
+                  style={tw`w-6 h-6 rounded-full `}
                 />
 
                 <TextInput
-                  style={tw`flex-1 text-lg text-black dark:text-white `}
-                  placeholder="Add a comment.."
+                  style={tw`flex-1  text-textPrimary dark:text-white `}
+                  placeholder="Write a comment...."
+                  placeholderTextColor={"#121212"}
                   onChangeText={(text) => setComment(text)}
                 />
               </View>
 
               {/* Eye icon */}
-              <Feather
-                name="send"
+              <Ionicons
+                name="send-sharp"
                 size={20}
-                style={tw`mr-2 text-[#00C49A]`}
-                color="black"
+                //   style={tw`mr-2 text-[#00C49A]`}
+                color="#ED6237"
                 onPress={handleComment}
               />
             </View>

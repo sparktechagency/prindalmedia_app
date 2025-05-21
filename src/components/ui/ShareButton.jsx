5ccfd93d2439be18b9ddc35f6b5c933df87477f7
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-21 10:37:13 +0600

    update iocns

--- src/components/ui/ShareButton.jsx
+++ src/components/ui/ShareButton.jsx
@@ -1,17 +1,16 @@
-import EvilIcons from "@expo/vector-icons/EvilIcons";
-import React from "react";
+import { IconSherd } from "@/assets/Icon";
 import { Alert, Share, TouchableOpacity } from "react-native";
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import tw from 'twrnc'
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SvgXml } from "react-native-svg";
+
+import tw from "twrnc";
 
 const ShareButton = () => {
   const onShare = async () => {
     try {
       await Share.share({
-        message:
-        'https://reactnative.dev/docs/touchableopacity'
+        message: "https://reactnative.dev/docs/touchableopacity",
       });
-      
     } catch (error) {
       Alert.alert(error.message);
     }
@@ -19,8 +18,9 @@ const ShareButton = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <TouchableOpacity onPress={onShare} title="Share"  style={tw`w-10`}>
-          <EvilIcons name="share-google" size={25} color="black" />
+        <TouchableOpacity onPress={onShare} title="Share" style={tw`w-10`}>
+          {/* <EvilIcons name="share-google" size={25} color="black" /> */}
+          <SvgXml xml={IconSherd} />
         </TouchableOpacity>
       </SafeAreaView>
     </SafeAreaProvider>

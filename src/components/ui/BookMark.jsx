fe9f14ec238c2e75700cc3a5518389a80ecdc9bc
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-21 09:25:27 +0600

    added the book's mark new Icons and alert

--- src/components/ui/BookMark.jsx
+++ src/components/ui/BookMark.jsx
@@ -1,24 +1,47 @@
-import Entypo from '@expo/vector-icons/Entypo';
+import { IconBookMark, IconBookMarkFull } from "@/assets/Icon";
 import { useState } from "react";
-import { View, Text } from "react-native";
+import { TouchableOpacity, View } from "react-native";
+import AwesomeAlert from "react-native-awesome-alerts";
+import { SvgXml } from "react-native-svg";
 import tw from "twrnc";
 
 const BookMark = () => {
   const [isBookMark, setIsBookMark] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
-  const handleBookMark = () => setIsBookMark(!isBookMark);
+  const handleBookmark = () => {
+    setIsBookMark(!isBookMark);
+    setShowAlert(true);
+  };
 
   return (
     <View>
-      {/* heart icon */}
-      <View style={tw`flex-row gap-1 items-center `}>
-        <Entypo
-          onPress={handleBookMark}
-          name="bookmark"
-          size={20}
-          color={isBookMark ? "red" : "black"}
-        />
+      <View style={tw`flex-row gap-1 items-center`}>
+        <TouchableOpacity onPress={handleBookmark}>
+          {isBookMark ? (
+            <SvgXml xml={IconBookMarkFull} />
+          ) : (
+            <SvgXml xml={IconBookMark} />
+          )}
+        </TouchableOpacity>
       </View>
+
+      <AwesomeAlert
+        show={showAlert}
+        showProgress={false}
+        title={isBookMark ? "Bookmark Added" : "Bookmark Removed"}
+        message={
+          isBookMark
+            ? "This item was removed from your bookmarks."
+            : "This item was added to your bookmarks."
+        }
+        closeOnTouchOutside={true}
+        closeOnHardwareBackPress={false}
+        showConfirmButton={true}
+        confirmText="OK"
+        confirmButtonColor="#0063E5"
+        onConfirmPressed={() => setShowAlert(false)}
+      />
     </View>
   );
 };

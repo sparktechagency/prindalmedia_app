398a661e5455fa1c5d296fef9b6ee5301f667156
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-22 12:47:55 +0600

    added new icons

--- src/components/ui/BackButton.jsx
+++ src/components/ui/BackButton.jsx
@@ -1,16 +1,14 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import { useNavigation } from "expo-router";
+import { IconBackButton } from "@/assets/Icon";
 import { Text, TouchableOpacity, View } from "react-native";
+import { SvgXml } from "react-native-svg";
 import tw from "../../lib/tailwind";
 
-const BackButton = ({ title }) => {
-  const navigate = useNavigation();
-
+const BackButton = ({ title, onPress }) => {
   return (
     <View style={tw`px-[4%] pt-4  mb-6`}>
-      <TouchableOpacity onPress={() => navigate.goBack()}>
+      <TouchableOpacity onPress={onPress}>
         <View style={tw`flex-row items-center gap-2 my-4`}>
-          <MaterialIcons name="arrow-back-ios" size={24} color="black" />
+          <SvgXml xml={IconBackButton} />
           <Text style={tw`text-5 text-[#121212] font-inter-700`}>{title}</Text>
         </View>
       </TouchableOpacity>

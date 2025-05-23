dfcc097e703f5564965c62a915559790db6e8393
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-23 15:08:07 +0600

    added back button

--- src/app/(drawer)/PrivacyPolicy.jsx
+++ src/app/(drawer)/PrivacyPolicy.jsx
@@ -1,22 +1,18 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import { useNavigation } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
-import tw from 'twrnc';
+import { router } from "expo-router";
+import { Text, View } from "react-native";
+import BackButton from "../../components/ui/BackButton";
+import tw from "../../lib/tailwind";
 
 const PrivacyPolicy = () => {
-    const navigate = useNavigation();
-
   return (
-    <View style={tw`p-[6%] `}>
-      <TouchableOpacity onPress={() => navigate.goBack()}>
-        <View style={tw`flex-row items-center gap-2 my-4`}>
-          <MaterialIcons name="arrow-back-ios" size={24} color="black" />
-          <Text style={tw`text-4.2 text-[#121212] font-bold`}>
-            Privacy policy
-          </Text>
-        </View>
-      </TouchableOpacity>
-      <Text style={tw``}>
+    <View style={tw` flex-1 bg-primaryBg`}>
+      <BackButton
+        onPress={() => {
+          router?.back();
+        }}
+        title={" Privacy policy"}
+      />
+      <Text style={tw`px-4 leading-5 font-inter-400`}>
         Lorem ipsum dolor sit amet consectetur. Pretium aliquam diam sed neque
         purus. Sed risus et turpis laoreet aliquet facilisis. Viverra urna arcu
         faucibus at ut orci velit. Velit pellentesque lacus eu tortor eget

d86c225d1f10eea7ec1b67b77c590417adac1bfd
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-24 09:37:36 +0600

    added the backbutton ,  handleSubmi , updated style

--- src/app/(drawer)/ChangePassword.jsx
+++ src/app/(drawer)/ChangePassword.jsx
@@ -1,18 +1,14 @@
+import { IconsChange } from "@/assets/Icon";
 import { AntDesign, Feather } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { router } from "expo-router";
 import { Formik } from "formik";
 import { useState } from "react";
-import {
-  Image,
-  Pressable,
-  ScrollView,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
+import { SvgXml } from "react-native-svg";
 import * as Yup from "yup";
 import BackButton from "../../components/ui/BackButton";
 import tw from "../../lib/tailwind";
+
 //  Yup validation schema
 const validationSchema = Yup.object().shape({
   currentPassword: Yup.string()
@@ -29,7 +25,6 @@ const validationSchema = Yup.object().shape({
 });
 
 const ChangePassword = () => {
-  const navigate = useNavigation();
   const [showCurrent, setShowCurrent] = useState(false);
   const [showNew, setShowNew] = useState(false);
   const [showConfirm, setShowConfirm] = useState(false);
@@ -39,6 +34,20 @@ const ChangePassword = () => {
     // Handle API call or navigation
   };
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleSubmi = async () => {
+    try {
+      setIsSubmitting(true);
+      setError(null);
+      // await submitFunction();
+    } catch (err) {
+      setError(err?.message || "Something went wrong");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
     <Formik
       initialValues={{
@@ -57,142 +66,144 @@ const ChangePassword = () => {
         errors,
         touched,
       }) => (
-        <View style={tw` bg-primaryBg  h-full`}>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={tw`p-[4%] flex-1  `}>
-              {/* Back and Title */}
-              <BackButton title={"Change password"} />
+        <View style={tw` bg-primaryBg flex-1 `}>
+          <BackButton
+            onPress={() => {
+              router?.back();
+            }}
+            title={"Change password"}
+          />
+          {/* <ScrollView showsVerticalScrollIndicator={false}> */}
+          <View style={tw`p-[4%]  flex-1    `}>
+            {/* Back and Title */}
+            {/* Image */}
+            <View style={tw`items-center`}>
+              <SvgXml xml={IconsChange} />
+            </View>
 
-              <View style={tw`flex-col h-full  justify-between  `}>
-                <View>
-                  {/* Image */}
-                  <View style={tw`items-center`}>
-                    <Image
-                      source={require("../../../assets/images/changePassword.png")}
-                      style={tw`w-48 h-42`}
+            <View style={tw` flex-col flex-1  justify-between`}>
+              {/* Inputs */}
+              <View style={tw`flex-col gap-4 mt-5`}>
+                {/* Current Password */}
+                <View style={tw`flex-col gap-2`}>
+                  <Text style={tw`text-3.5 text-[#121212] font-inter-600 `}>
+                    Current Password
+                  </Text>
+                  <View
+                    style={tw`flex-row items-center bg-[#f3f3f3] rounded-2 px-4 py-2`}
+                  >
+                    <AntDesign
+                      name="lock"
+                      size={20}
+                      style={tw`mr-2 text-[#888888]`}
+                    />
+                    <TextInput
+                      secureTextEntry={!showCurrent}
+                      style={tw`flex-1 text-base text-black`}
+                      onChangeText={handleChange("currentPassword")}
+                      onBlur={handleBlur("currentPassword")}
+                      value={values.currentPassword}
+                    />
+                    <Feather
+                      onPress={() => setShowCurrent(!showCurrent)}
+                      name={showCurrent ? "eye-off" : "eye"}
+                      size={18}
+                      style={tw`text-[#888888]`}
                     />
                   </View>
-
-                  {/* Inputs */}
-                  <View style={tw`flex-col gap-4 mt-5`}>
-                    {/* Current Password */}
-                    <View style={tw`flex-col gap-2`}>
-                      <Text style={tw`text-3.5 text-[#121212] font-inter-600 `}>
-                        Current Password
-                      </Text>
-                      <View
-                        style={tw`flex-row items-center bg-[#f3f3f3] rounded-2 px-4 py-2`}
-                      >
-                        <AntDesign
-                          name="lock"
-                          size={20}
-                          style={tw`mr-2 text-[#888888]`}
-                        />
-                        <TextInput
-                          secureTextEntry={!showCurrent}
-                          style={tw`flex-1 text-base text-black`}
-                          onChangeText={handleChange("currentPassword")}
-                          onBlur={handleBlur("currentPassword")}
-                          value={values.currentPassword}
-                        />
-                        <Feather
-                          onPress={() => setShowCurrent(!showCurrent)}
-                          name={showCurrent ? "eye-off" : "eye"}
-                          size={18}
-                          style={tw`text-[#888888]`}
-                        />
-                      </View>
-                      {touched.currentPassword && errors.currentPassword && (
-                        <Text style={tw`text-red-500 mt-1 ml-2 text-xs`}>
-                          {errors.currentPassword}
-                        </Text>
-                      )}
-                    </View>
-
-                    {/* New Password */}
-                    <View style={tw`flex-col gap-2`}>
-                      <Text style={tw`text-3.5 text-[#121212] font-inter-600 `}>
-                        New Password
-                      </Text>
-                      <View
-                        style={tw`flex-row items-center bg-[#f3f3f3] rounded-2 px-4 py-2`}
-                      >
-                        <AntDesign
-                          name="lock"
-                          size={20}
-                          style={tw`mr-2 text-[#888888]`}
-                        />
-                        <TextInput
-                          secureTextEntry={!showNew}
-                          style={tw`flex-1 text-base text-black`}
-                          onChangeText={handleChange("newPassword")}
-                          onBlur={handleBlur("newPassword")}
-                          value={values.newPassword}
-                        />
-                        <Feather
-                          onPress={() => setShowNew(!showNew)}
-                          name={showNew ? "eye-off" : "eye"}
-                          size={18}
-                          style={tw`text-[#888888]`}
-                        />
-                      </View>
-                      {touched.newPassword && errors.newPassword && (
-                        <Text style={tw`text-red-500 mt-1 ml-2 text-xs`}>
-                          {errors.newPassword}
-                        </Text>
-                      )}
-                    </View>
-
-                    {/* Confirm Password */}
-                    <View style={tw`flex-col gap-2`}>
-                      <Text style={tw`text-3.5 text-[#121212] font-inter-600 `}>
-                        Confirm Password
-                      </Text>
-                      <View
-                        style={tw`flex-row items-center bg-[#f3f3f3] rounded-2 px-4 py-2`}
-                      >
-                        <AntDesign
-                          name="lock"
-                          size={20}
-                          style={tw`mr-2 text-[#888888]`}
-                        />
-                        <TextInput
-                          secureTextEntry={!showConfirm}
-                          style={tw`flex-1 text-base text-black`}
-                          onChangeText={handleChange("confirmPassword")}
-                          onBlur={handleBlur("confirmPassword")}
-                          value={values.confirmPassword}
-                        />
-                        <Feather
-                          onPress={() => setShowConfirm(!showConfirm)}
-                          name={showConfirm ? "eye-off" : "eye"}
-                          size={18}
-                          style={tw`text-[#888888]`}
-                        />
-                      </View>
-                      {touched.confirmPassword && errors.confirmPassword && (
-                        <Text style={tw`text-red-500 mt-1 ml-2 text-xs`}>
-                          {errors.confirmPassword}
-                        </Text>
-                      )}
-                    </View>
-                  </View>
+                  {touched.currentPassword && errors.currentPassword && (
+                    <Text style={tw`text-red-500 mt-1 ml-2 text-xs`}>
+                      {errors.currentPassword}
+                    </Text>
+                  )}
                 </View>
 
-                {/* Submit Button */}
-                <View style={tw`items-center justify-center flex-row `}>
-                  <Pressable
-                    onPress={handleSubmit}
-                    style={tw`items-center justify-center flex-row bg-orange w-full rounded-full`}
+                {/* New Password */}
+                <View style={tw`flex-col gap-2`}>
+                  <Text style={tw`text-3.5 text-[#121212] font-inter-600 `}>
+                    New Password
+                  </Text>
+                  <View
+                    style={tw`flex-row items-center bg-[#f3f3f3] rounded-2 px-4 py-2`}
                   >
-                    <Text style={tw`text-white py-3 font-medium text-lg`}>
-                      Save changes
+                    <AntDesign
+                      name="lock"
+                      size={20}
+                      style={tw`mr-2 text-[#888888]`}
+                    />
+                    <TextInput
+                      secureTextEntry={!showNew}
+                      style={tw`flex-1 text-base text-black`}
+                      onChangeText={handleChange("newPassword")}
+                      onBlur={handleBlur("newPassword")}
+                      value={values.newPassword}
+                    />
+                    <Feather
+                      onPress={() => setShowNew(!showNew)}
+                      name={showNew ? "eye-off" : "eye"}
+                      size={18}
+                      style={tw`text-[#888888]`}
+                    />
+                  </View>
+                  {touched.newPassword && errors.newPassword && (
+                    <Text style={tw`text-red-500 mt-1 ml-2 text-xs`}>
+                      {errors.newPassword}
                     </Text>
-                  </Pressable>
+                  )}
+                </View>
+
+                {/* Confirm Password */}
+                <View style={tw`flex-col gap-2`}>
+                  <Text style={tw`text-3.5 text-[#121212] font-inter-600 `}>
+                    Confirm Password
+                  </Text>
+                  <View
+                    style={tw`flex-row items-center bg-[#f3f3f3] rounded-2 px-4 py-2`}
+                  >
+                    <AntDesign
+                      name="lock"
+                      size={20}
+                      style={tw`mr-2 text-[#888888]`}
+                    />
+                    <TextInput
+                      secureTextEntry={!showConfirm}
+                      style={tw`flex-1 text-base text-black`}
+                      onChangeText={handleChange("confirmPassword")}
+                      onBlur={handleBlur("confirmPassword")}
+                      value={values.confirmPassword}
+                    />
+                    <Feather
+                      onPress={() => setShowConfirm(!showConfirm)}
+                      name={showConfirm ? "eye-off" : "eye"}
+                      size={18}
+                      style={tw`text-[#888888]`}
+                    />
+                  </View>
+                  {touched.confirmPassword && errors.confirmPassword && (
+                    <Text style={tw`text-red-500 mt-1 ml-2 text-xs`}>
+                      {errors.confirmPassword}
+                    </Text>
+                  )}
                 </View>
               </View>
+
+              {/* Submit Button */}
+              <View style={tw`  `}>
+                <Pressable
+                  onPress={handleSubmi}
+                  disabled={isSubmitting}
+                  style={tw`items-center justify-center flex-row ${
+                    isSubmitting ? "bg-gray-400" : "bg-orange"
+                  } w-full rounded-full`}
+                >
+                  <Text style={tw`text-white py-3 font-medium text-lg`}>
+                    {isSubmitting ? "Saving..." : "Save changes"}
+                  </Text>
+                </Pressable>
+              </View>
             </View>
-          </ScrollView>
+          </View>
+          {/* </ScrollView> */}
         </View>
       )}
     </Formik>

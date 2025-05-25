a77881b0580cc85514e968c7cfa4e3c9d051906f
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-25 07:47:07 +0600

    Updated the new icons

--- src/app/(drawer)/(tab)/Profile.jsx
+++ src/app/(drawer)/(tab)/Profile.jsx
@@ -1,7 +1,8 @@
 // ProfileCard.tsx
+import { IconRecents, IconUserProfile, Iconsfollower } from "@/assets/Icon";
 import { Link } from "expo-router";
 import { Image, Text, View } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { SvgXml } from "react-native-svg";
 import RecentActivityList from "../../../components/ui/RecentActivityList";
 import tw from "../../../lib/tailwind";
 
@@ -18,7 +19,7 @@ const Profile = () => {
           {/* Profile image and badge */}
           <View style={tw`items-center relative`}>
             <Image
-              source={{ uri: "https://randomuser.me/api/portraits/men/32.jpg" }} // Replace with your image
+              source={require("@/assets/images/image.png")} // Replace with your image
               style={tw`w-20 h-20 rounded-full`}
             />
             <Link
@@ -26,7 +27,8 @@ const Profile = () => {
               href={"/provers/1"}
             >
               <View>
-                <Icon name="edit" size={18} color="#fff" />
+                {/* <Icon name="edit" size={18} color="#fff" /> */}
+                <SvgXml xml={IconUserProfile} />
               </View>
             </Link>
           </View>
@@ -49,9 +51,9 @@ const Profile = () => {
           <View style={tw`flex-row mt-4  justify-center  gap-2`}>
             <Link href={"/userfollowing"}>
               <View
-                style={tw`items-center justify-center flex-row gap-1.3  p-2 rounded-2 px-6 `}
+                style={tw`items-center  justify-center flex-row gap-1.3  p-2 rounded-2 px-6 `}
               >
-                <Icon name="users" size={18} color="#121212" />
+                <SvgXml xml={Iconsfollower} />
                 <Text style={tw` font-inter-400 `}>Followers:</Text>
                 <Text style={tw`text-textPrimary font-inter-700`}>5.1k</Text>
               </View>
@@ -61,7 +63,7 @@ const Profile = () => {
               <View
                 style={tw`items-center justify-center flex-row gap-1.3  p-2 rounded-2 px-6 `}
               >
-                <Icon name="users" size={18} color="#121212" />
+                <SvgXml xml={Iconsfollower} />
                 <Text style={tw` font-inter-400 `}>Following:</Text>
                 <Text style={tw`text-textPrimary font-inter-700`}>1.1k</Text>
               </View>
@@ -73,6 +75,7 @@ const Profile = () => {
         <View style={tw`flex-1`}>
           <View style={tw` `}>
             <View style={tw`flex-row items-center gap-2 my-4`}>
+              <SvgXml xml={IconRecents} />
               <Text style={tw`text-4 text-[#121212] font-inter-700`}>
                 Recent activity list
               </Text>

203202f8b554ce2bc87ff20817f7bb2cbc49eb72
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-27 15:46:37 +0600

    updated tabs style

--- src/app/(drawer)/(tab)/Bookmarks.jsx
+++ src/app/(drawer)/(tab)/Bookmarks.jsx
@@ -1,59 +1,101 @@
 import { useState } from "react";
 import { Pressable, Text, View } from "react-native";
 // import Icon from "react-native-vector-icons/FontAwesome";
+import {
+  IconsListRecipes,
+  IconsListRestaurants,
+  IconsSearch
+} from "@/assets/Icon";
+import { TextInput } from "react-native-gesture-handler";
+import { SvgXml } from "react-native-svg";
 import Icons from "react-native-vector-icons/MaterialCommunityIcons";
-import Recent from "../../../components/ui/Recent";
+import MyList from "../../../components/ui/MyList";
 import tw from "../../../lib/tailwind";
 
 // tabs name and icons
 const tabs = [
   { label: "Restaurants", icon: "history" },
-  { label: "Recipes", icon: "account-group-outline" },
+  { label: "Home-Made", icon: "account-group-outline" },
 ];
 
 const Bookmarks = () => {
   const [activeTab, setActiveTab] = useState("Restaurants");
+  //
 
   return (
     <View style={tw`flex-1 bg-[#FDFFFE] `}>
       <View style={tw`flex-1 p-[4%]  `}>
-        <Text style={tw`text-xl font-inter-700 p-4`}>Lists</Text>
+        <Text style={tw`text-xl font-inter-700 p-4`}>My Lists</Text>
 
-        {/* added the all tabs view components */}
-        <View style={tw`flex-1 `}>
-          {activeTab === "Restaurants" && <Recent />}
-          {activeTab === "Recipes" && <Recent />}
-        </View>
+        {/*  */}
 
-        {/* Tabs */}
-        <View style={tw` mt-2 flex-row  items-center justify-center `}>
-          <View
-            style={tw` flex-row  items-center p-2 justify-center  bg-[#3333331A] rounded-full `}
-          >
-            {tabs.map((item, index) => (
-              <Pressable
-                key={item.label}
-                onPress={() => setActiveTab(item.label)}
-                style={tw`flex-row items-center px-2 py-2 rounded-full ${
-                  activeTab === item.label ? "bg-orange" : ""
-                }`}
+        <View style={tw` flex-1 flex-col gap-4 justify-start`}>
+          <View style={tw` flex-col gap-5 justify-start`}>
+            <View style={tw`my-2`}>
+              <View
+                style={tw`bg-[#F3F3F3] flex-row items-center justify-between px-2 rounded-4 gap-1`}
               >
-                {/* view icons  */}
-                <Icons
-                  size={20}
-                  color={activeTab === item.label ? "white" : "#454545"}
-                  style={tw`mr-2`}
+                <SvgXml xml={IconsSearch} />
+
+                <TextInput
+                  style={tw` py-4 flex-1 text-textPrimary`}
+                  placeholder="Search by user name or food name"
+                  placeholderTextColor="#888888"
+                  // onChangeText={(text) => handleSearchLocation(text)}
+                  // value={searchText}
                 />
-                {/* tabs name and bg change  */}
-                <Text
-                  style={tw`${
-                    activeTab === item.label ? "text-white" : "text-[#454545]"
-                  } font-medium`}
-                >
-                  {item.label}
-                </Text>
-              </Pressable>
-            ))}
+              </View>
+            </View>
+
+            {/* Tabs */}
+            <View style={tw`flex-row items-center justify-between `}>
+              <View style={tw` flex-row  items-center gap-2 justify-center   `}>
+                {tabs.map((item, index) => (
+                  <Pressable
+                    key={item.label}
+                    onPress={() => setActiveTab(item.label)}
+                    style={tw`flex-row items-center gap-2  rounded-full ${
+                      activeTab === item.label ? " text-orange" : ""
+                    }`}
+                  >
+                    {/* view icons  */}
+                    <Icons
+                      size={20}
+                      color={activeTab === item.label ? "white" : "#454545"}
+                      style={tw`mr-2`}
+                    />
+                    {/* tabs name and bg change  */}
+                    <Text
+                      style={tw`${
+                        activeTab === item.label
+                          ? "text-orange underline "
+                          : "text-[#454545] "
+                      }  text-4 font-inter-600 gap-2`}
+                    >
+                      {item.label}
+                    </Text>
+                  </Pressable>
+                ))}
+              </View>
+              <View style={tw`flex-row items-center  ml-3 gap-2 `}>
+                <SvgXml
+                  xml={
+                    activeTab === "Restaurants"
+                      ? IconsListRestaurants
+                      : IconsListRecipes
+                  }
+                />
+                <Text>{activeTab === "Restaurants" ? 257 : 258} </Text>
+              </View>
+            </View>
+          </View>
+
+          {/* added the all tabs view components */}
+          <View style={tw`flex-1 `}>
+            {/* {activeTab === "Restaurants" && <Recent />}
+            {activeTab === "Recipes" && <Recent />} */}
+            {activeTab === "Restaurants" && <MyList />}
+            {activeTab === "Recipes" && <MyList />}
           </View>
         </View>
       </View>

89323db7059f3f0b5d99acaf2bf8a4d0358c9dec
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-27 15:47:09 +0600

    updated height

--- src/app/(drawer)/(tab)/_layout.jsx
+++ src/app/(drawer)/(tab)/_layout.jsx
@@ -9,73 +9,83 @@ import {
   IconProfileinActive,
 } from "@/assets/Icon";
 import { Tabs } from "expo-router";
+import { View } from "react-native";
 import { SvgXml } from "react-native-svg";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          headerShown: false,
-          title: "Home",
-          tabBarActiveTintColor: "#C3570E",
-          tabBarInactiveTintColor: "#454545",
-          tabBarIcon: ({ focused }) => (
-            <SvgXml
-              xml={focused ? IconActive : IconHome}
-              width={28}
-              height={28}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Post"
-        options={{
-          title: "Post",
-          tabBarActiveTintColor: "#C3570E",
-          tabBarInactiveTintColor: "#454545",
-          tabBarIcon: ({ focused }) => (
-            <SvgXml
-              xml={focused ? IconPostinActive : IconPostActive}
-              width={28}
-              height={28}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Bookmarks"
-        options={{
-          title: "Lists",
-          tabBarActiveTintColor: "#C3570E",
-          tabBarInactiveTintColor: "#454545",
-          tabBarIcon: ({ focused }) => (
-            <SvgXml
-              xml={focused ? IconListActive : IconListinActive}
-              width={28}
-              height={28}
-            />
-          ),
-        }}
-      />
+    <Tabs
+  screenOptions={{
+    headerShown: false,
+    tabBarActiveTintColor: '#ED6237',
+    tabBarInactiveTintColor: '#454545',
+    tabBarStyle: {
+      paddingVertical: 10, 
+      height: 70,         
+    },
+  }}
+>
+  <Tabs.Screen
+    name="index"
+    options={{
+      title: "Home",
+      tabBarIcon: ({ focused }) => (
+        <View style={{ marginTop: 4 }}>
+          <SvgXml
+            xml={focused ? IconActive : IconHome}
+            width={28}
+            height={28}
+          />
+        </View>
+      ),
+    }}
+  />
+  <Tabs.Screen
+    name="Post"
+    options={{
+      title: "Post",
+      tabBarIcon: ({ focused }) => (
+        <View style={{ marginTop: 4 }}>
+          <SvgXml
+            xml={focused ? IconPostinActive : IconPostActive}
+            width={28}
+            height={28}
+          />
+        </View>
+      ),
+    }}
+  />
+  <Tabs.Screen
+    name="Bookmarks"
+    options={{
+      title: "Lists",
+      tabBarIcon: ({ focused }) => (
+        <View style={{ marginTop: 4 }}>
+          <SvgXml
+            xml={focused ? IconListActive : IconListinActive}
+            width={28}
+            height={28}
+          />
+        </View>
+      ),
+    }}
+  />
+  <Tabs.Screen
+    name="Profile"
+    options={{
+      title: "Profile",
+      tabBarIcon: ({ focused }) => (
+        <View style={{ marginTop: 4 }}>
+          <SvgXml
+            xml={focused ? IconProfileActive : IconProfileinActive}
+            width={28}
+            height={28}
+          />
+        </View>
+      ),
+    }}
+  />
+</Tabs>
 
-      <Tabs.Screen
-        name="Profile"
-        options={{
-          title: "Profile",
-          tabBarActiveTintColor: "#C3570E",
-          tabBarInactiveTintColor: "#454545",
-          tabBarIcon: ({ focused }) => (
-            <SvgXml
-              xml={focused ? IconProfileActive : IconProfileinActive}
-              width={28}
-              height={28}
-            />
-          ),
-        }}
-      />
-    </Tabs>
   );
 }

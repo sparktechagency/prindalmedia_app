ded3c805560103032eb922d45a3700b2828c2e7a
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-29 14:09:44 +0600

    updare text style

--- src/app/(drawer)/Mission.jsx
+++ src/app/(drawer)/Mission.jsx
@@ -8,7 +8,7 @@ const Mission = () => {
       <View style={tw`flex-row items-center gap-2 my-4`}>
         <TouchableOpacity
           onPress={() => {
-            router?.back();
+            router.back();
           }}
         >
           <MaterialIcons name="arrow-back-ios" size={24} color="black" />
@@ -17,9 +17,7 @@ const Mission = () => {
       </View>
 
       <View style={tw` flex-col gap-4 mt-6 `}>
-        <Text
-          style={tw` leading-6 text-4 font-inter-500  text-[#454545]`}
-        >
+        <Text style={tw` leading-6 text-4 font-inter-500  text-[#454545]`}>
           Great food can be found everywhere from fine dining rooms to
           late-night takeout, home-cooked meals to street food gems. But what
           truly makes a meal memorable isn’t just the taste it’s the people you
@@ -31,9 +29,7 @@ const Mission = () => {
           At our core, we believe food is a way to connect. A way to tell
           stories, pass down traditions, and bring people closer.
         </Text>
-        <Text
-          style={tw` leading-6 text-4 font-inter-500  text-[#454545]`}
-        >
+        <Text style={tw` leading-6 text-4 font-inter-500  text-[#454545]`}>
           That’s why we built this app to help you share your taste and capture
           the stories that come with every bite.
         </Text>

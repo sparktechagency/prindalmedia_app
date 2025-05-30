515fdc2adee062eadc4c17cd89e3d6b76f5e4685
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-30 10:25:05 +0600

    removed the old style and added the new components

--- src/components/ui/SelectRadioButton.jsx
+++ src/components/ui/SelectRadioButton.jsx
@@ -1,59 +1,69 @@
-import { Text, View } from "react-native";
-import { RadioButton } from "react-native-ui-lib";
+import { Text, TextInput, View } from "react-native";
 import tw from "../../lib/tailwind";
+import CustomRadioButton from "./CustomRadioButton";
 
 const SelectRadioButton = ({
   setSelectedOption,
   selectedOption,
   setSelectedOptionFood,
   selectedOptionFood,
+  setRestaurant,
 }) => {
+  // console.log(selectedOption);
+
   return (
-    <View style={tw`flex-col  gap-4`}>
+    <View style={tw`flex-col gap-4`}>
       <View style={tw`gap-2`}>
         <Text style={tw`text-[16px] font-inter-600 text-[#121212]`}>
           Where did you have it?
         </Text>
         <View style={tw`flex-row gap-6`}>
-          <RadioButton
+          <CustomRadioButton
             label="Restaurant"
-            labelStyle={{ color: "#454545", fontWeight: "400" }}
             selected={selectedOption === "restaurant"}
             onPress={() => setSelectedOption("restaurant")}
-            color="#B0B0B0"
           />
-
-          <RadioButton
+          <CustomRadioButton
             label="Home-Made"
-            labelStyle={{ color: "#454545", fontWeight: "400" }}
             selected={selectedOption === "home-made"}
             onPress={() => setSelectedOption("home-made")}
-            color="#B0B0B0"
           />
         </View>
       </View>
+
+      <>
+        {selectedOption === "restaurant" && (
+          <View style={tw`flex-col gap-2`}>
+            <Text style={tw`text-[16px] font-inter-600 text-[#121212]`}>
+              Restaurant Name
+            </Text>
+            <View style={tw`bg-[#F3F3F3] rounded-md text-[#121212] flex-1`}>
+              <TextInput
+                placeholder="What’s the restaurant name?"
+                style={tw`px-4 py-4 flex-1 text-black`}
+                placeholderTextColor={"#888888"}
+                onChangeText={(text) => setRestaurant(text)}
+                // value={mealName}
+              />
+            </View>
+          </View>
+        )}
+      </>
       <View style={tw`gap-2`}>
-        <Text style={tw`text-[16px] font-inter-600 text-[#121212`}>
+        <Text style={tw`text-[16px] font-inter-600 text-[#121212]`}>
           Food type?
         </Text>
-        <View style={tw`flex-row gap-2`}>
-          <View style={tw`flex-row gap-6`}>
-            <RadioButton
-              label="Meal"
-              labelStyle={{ color: "#454545", fontWeight: "400" }}
-              selected={selectedOptionFood === "meal"}
-              onPress={() => setSelectedOptionFood("meal")}
-              color="#B0B0B0"
-            />
-
-            <RadioButton
-              label="Drink"
-              labelStyle={{ color: "#454545", fontWeight: "400" }}
-              selected={selectedOptionFood === "drink"}
-              onPress={() => setSelectedOptionFood("drink")}
-              color="#B0B0B0"
-            />
-          </View>
+        <View style={tw`flex-row gap-6`}>
+          <CustomRadioButton
+            label="Meal"
+            selected={selectedOptionFood === "meal"}
+            onPress={() => setSelectedOptionFood("meal")}
+          />
+          <CustomRadioButton
+            label="Drink"
+            selected={selectedOptionFood === "drink"}
+            onPress={() => setSelectedOptionFood("drink")}
+          />
         </View>
       </View>
     </View>

f20004189da4e875ec71bfdad2620242f5109878
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-30 10:28:27 +0600

    removed some old designs and added the handler navigate

--- src/components/ui/RecentActivityList.jsx
+++ src/components/ui/RecentActivityList.jsx
@@ -1,12 +1,18 @@
+import { IconRestruernt } from "@/assets/Icon";
 import { FontAwesome } from "@expo/vector-icons";
+import { router } from "expo-router";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SvgXml } from "react-native-svg";
 import foodImage from "../../../assets/images/food-image.png";
-import Icon from "../../../assets/images/restaurant.png";
 import tw from "../../lib/tailwind";
 
 const DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const RecentActivityList = () => {
+  const handleNavigate = () => {
+    // console.log("asdfg");
+    router.push(`/notifications/${1}`);
+  };
   return (
     <View style={tw`flex-1`}>
       {/* when the api changes ScrollView and adds flatList  */}
@@ -16,7 +22,7 @@ const RecentActivityList = () => {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={tw`pb-6`}
         renderItem={({ item, index }) => (
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleNavigate}>
             <View style={tw`flex-col my-2 justify-between items-center`}>
               <View
                 style={tw`flex-row gap-4 bg-[#D5D5D51A] p-2 rounded-2xl items-center`}
@@ -25,36 +31,43 @@ const RecentActivityList = () => {
                 <Image source={foodImage} style={tw`w-18 h-18 rounded-[8px]`} />
 
                 {/* Content */}
-                <View style={tw`flex-1 px-2`}>
+                <View style={tw`flex-1 `}>
                   {/* Title and Rating */}
-                  <View style={tw`flex-row justify-between items-center`}>
-                    <Text
-                      style={tw`text-base  font-inter-700 text-textPrimary`}
+
+                  <View style={tw`flex-row   justify-start items-start`}>
+                    <View style={tw`flex-col gap-1.2 justify-between `}>
+                      <Text
+                        style={tw`text-base  font-inter-700 text-textPrimary`}
+                      >
+                        Spaghetti Carbonara
+                      </Text>
+                      <View style={tw`flex-row items-center`}>
+                        <SvgXml xml={IconRestruernt} />
+                        <Text
+                          style={tw`text-[#454545] ml-1 flex items-center justify-center font-inter-400 text-sm`}
+                        >
+                          78 Tampa,Florida
+                        </Text>
+                      </View>
+                    </View>
+
+                    {/* Location and Date */}
+                    <View
+                      style={tw`flex-col gap-1.2 mt-1 justify-between items-end`}
                     >
-                      Spaghetti Carbonara
-                    </Text>
-                    <View style={tw`flex-row items-center`}>
-                      <FontAwesome name="star" size={16} color="#facc15" />
-                      <Text style={tw`ml-1 text-textPrimary font-inter-600 `}>
-                        2.8
+                      <View style={tw`flex-row items-center`}>
+                        <FontAwesome name="star" size={16} color="#facc15" />
+                        <Text style={tw`ml-1 text-textPrimary font-inter-600 `}>
+                          2.8
+                        </Text>
+                      </View>
+
+                      <Text style={tw`text-[#454545] font-inter-400 text-sm`}>
+                        13th May
                       </Text>
                     </View>
                   </View>
 
-                  {/* Location and Date */}
-                  <View style={tw`flex-row justify-between items-center mt-1`}>
-                    <View style={tw`flex-row items-center`}>
-                      <Image style={tw`w-4 h-4`} source={Icon} />
-                      <Text
-                        style={tw`text-[#454545] ml-1 font-inter-400 text-sm`}
-                      >
-                        78 Tampa, Florida
-                      </Text>
-                    </View>
-                    <Text style={tw`text-[#454545] font-inter-400 text-sm`}>
-                      07/04/2027
-                    </Text>
-                  </View>
                   {/* Tags */}
                   <View style={tw`flex-row justify-between items-center mt-2`}>
                     <View style={tw`flex-row gap-2`}>

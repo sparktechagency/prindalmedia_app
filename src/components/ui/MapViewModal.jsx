fa83ab0cbe0da7900eef5f4d085615ad20adfe3a
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-30 10:46:55 +0600

    MapViewModal page

---
+++ src/components/ui/MapViewModal.jsx
@@ -0,0 +1,49 @@
+import { AntDesign } from "@expo/vector-icons";
+import { Modal, Pressable, Text, TouchableOpacity, View } from "react-native";
+import tw from "../../lib/tailwind";
+
+const MapViewModal = ({ isVisible, setIsVisible }) => {
+  return (
+    <View style={tw`flex-1 justify-center items-center`}>
+      {/* Trigger Button */}
+      <TouchableOpacity
+        onPress={() => setIsVisible(true)}
+        style={tw`bg-black px-4 py-3 rounded-lg`}
+      >
+        <Text style={tw`text-white font-bold`}>Show Alert</Text>
+      </TouchableOpacity>
+
+      {/* Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isVisible}
+        onRequestClose={() => setIsVisible(false)}
+      >
+        <View style={tw`flex-1 bg-black/40 justify-center items-center`}>
+          <View style={tw`bg-white w-80 rounded-xl p-6 items-center`}>
+            <View style={tw`bg-gray-200 p-4 rounded-full mb-4`}>
+              <AntDesign name="checkcircle" size={32} color="black" />
+            </View>
+
+            <Text style={tw`text-lg font-bold mb-2 text-black`}>
+              Successful!
+            </Text>
+            <Text style={tw`text-sm text-gray-500 mb-5 text-center`}>
+              Your post was successful
+            </Text>
+
+            <Pressable
+              onPress={() => setIsVisible(false)}
+              style={tw`bg-black py-3 rounded-full w-full items-center`}
+            >
+              <Text style={tw`text-white font-bold`}>Done</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default MapViewModal;

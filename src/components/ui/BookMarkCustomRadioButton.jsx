f584ff3650f020407394418fa988f0a1e04b8537
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-30 10:39:57 +0600

    ceated the BookMarkCustomRadioButton page

---
+++ src/components/ui/BookMarkCustomRadioButton.jsx
@@ -0,0 +1,21 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import tw from "../../lib/tailwind";
+
+const BookMarkCustomRadioButton = ({ label, selected, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={tw`flex-row items-center gap-2`}>
+      <View
+        style={[
+          tw`w-5 h-5 rounded-full border ${
+            selected ? "border-orange" : "border-[#B0B0B0]"
+          }  items-center justify-center`,
+        ]}
+      >
+        {selected && <View style={tw`w-3 h-3 rounded-full bg-orange`} />}
+      </View>
+      <Text style={tw`text-[#454545] font-normal`}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default BookMarkCustomRadioButton;

3dc8d9eaa8df4badbddcd8a53aa9569868794b2f
Tanvir islam <tanvirislam3912@gmail.com>
2025-05-31 11:37:08 +0600

    locations marker

---
+++ src/utils/marker.js
@@ -0,0 +1,37 @@
+export const newMarker = [
+  {
+    id: 1,
+    name: "Banasree",
+    latitude: 23.7558,
+    longitude: 90.4229,
+    type: "residential_area",
+  },
+  {
+    id: 2,
+    name: "Bashundhara City Mall",
+    latitude: 23.7514,
+    longitude: 90.391,
+    type: "shopping_mall",
+  },
+  {
+    id: 3,
+    name: "Square Hospital",
+    latitude: 23.7528,
+    longitude: 90.3883,
+    type: "hospital",
+  },
+  {
+    id: 4,
+    name: "Hazrat Shahjalal International Airport",
+    latitude: 23.8434,
+    longitude: 90.3978,
+    type: "airport",
+  },
+  {
+    id: 5,
+    name: "Dhaka University",
+    latitude: 23.733,
+    longitude: 90.3925,
+    type: "university",
+  },
+];

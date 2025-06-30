f5f5f34869bf06022c68280ff97ac3079d689bb4
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-30 15:08:58 +0600

    refactor Logout component to implement logout confirmation and token deletion logic

--- src/components/ui/Logout.jsx
+++ src/components/ui/Logout.jsx
@@ -1,13 +1,37 @@
 import { IconLogOut } from "@/assets/Icon";
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { SvgXml } from "react-native-svg";
 import tw from "../../lib/tailwind";
+import { storage } from "../../utils/storage";
 
 const Logout = () => {
+  const handleLogout = () => {
+    Alert.alert("Confirm Logout", "Are you sure you want to log out?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Log out",
+        style: "destructive",
+        onPress: async () => {
+          await storage.delete("token");
+          router.replace("/auth"); // আপনি যেই স্ক্রীনে নিতে চান logout এর পরে
+        },
+      },
+    ]);
+  };
+
   return (
-    <View style={tw`flex-row items-center gap-2`}>
-      <SvgXml xml={IconLogOut} />
-      <Text style={tw` text-[#E53E3E]`}>Log out</Text>
+    <View>
+      <TouchableOpacity
+        onPress={handleLogout}
+        style={tw`flex-row items-center gap-2`}
+      >
+        <SvgXml xml={IconLogOut} />
+        <Text style={tw`text-[#E53E3E] font-medium`}>Log out</Text>
+      </TouchableOpacity>
     </View>
   );
 };
